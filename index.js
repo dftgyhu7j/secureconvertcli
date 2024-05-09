d201a631aa4b585e9de9adbd6a6fceefdf19dbee
dftgyhu7j <gs1landonh7@hotmail.com>
2024-05-09 20:08:08 +0700

    commit

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-function permute(nums) {
-  const result = [];
-  backtrack([]);
-  return result;
-  function backtrack(current) {
-    if (current.length === nums.length) {
-      result.push([...current]);
-      return;
-    }
-    for (const num of nums) {
-      if (current.includes(num)) continue;
-      current.push(num);
-      backtrack(current);
-      current.pop();
-    }
-  }
-}
+const head = (arr) => arr[0];
