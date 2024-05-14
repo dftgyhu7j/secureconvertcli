2d3d77e9930e2f31e8534596280dd6636a49d8ef
dftgyhu7j <gs1landonh7@hotmail.com>
2024-05-14 20:34:28 +0700

    hi commit

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-function search(nums, target) {
-  let left = 0;
-  let right = nums.length - 1;
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-    if (nums[mid] === target) return mid;
-    if (nums[left] <= nums[mid]) {
-      if (nums[left] <= target && target < nums[mid]) right = mid - 1;
-      else left = mid + 1;
-    } else {
-      if (nums[mid] < target && target <= nums[right]) left = mid + 1;
-      else right = mid - 1;
-    }
-  }
-  return -1;
+function factorial(n) {
+  if (n === 0 || n === 1) return 1;
+  return n * factorial(n - 1);
 }
